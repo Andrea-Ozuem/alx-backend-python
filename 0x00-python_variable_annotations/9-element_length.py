bf29a59eebd5f5195774677d31571cdf6671b58b
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-05 19:49:01 +0100

    fix: func doc

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,5 @@ from typing import Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''itearable annotation'''
     return [(i, len(i)) for i in lst]
