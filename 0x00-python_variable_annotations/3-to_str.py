579428baa70da3428d5b58b0204a0e385b26aed5
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-05 18:12:00 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''a type-annotated function concat that takes a string str1
+and a string str2 as arguments and returns a concatenated string'''
+
+
+def to_str(n: float) -> str:
+    '''concats 2 str'''
+    return str(n)
