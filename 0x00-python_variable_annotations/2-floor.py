4cdbd796c074f8ccb3f33b3f4ae9e78304f89471
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-05 18:04:49 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import math
+
+'''a type-annotated function concat that takes a string str1
+and a string str2 as arguments and returns a concatenated string'''
+
+
+def floor(n: float) -> int:
+    '''concats 2 str'''
+    return int(math.floor(n))
