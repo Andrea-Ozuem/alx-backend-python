70e874dbc39fbee2ba7bb6ccb55264fbcdebc787
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-05 18:08:42 +0100

    fix: module doc requiremn

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import math
-
 '''a type-annotated function concat that takes a string str1
 and a string str2 as arguments and returns a concatenated string'''
+
+import math
 
 
 def floor(n: float) -> int:
