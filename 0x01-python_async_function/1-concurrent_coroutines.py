1e7f64679e6979dbb8d07adaba9e3e52cc06d15c
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-09 18:04:38 +0100

    fix: Use gather

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,4 +10,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''random, n times'''
-    return sorted([await wait_random(max_delay) for val in range(n)])
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,4 +2,10 @@
 'Measure runtime'
 
 
-import 
+import time
+
+def ddd:
+    start = time.time()
+    wait_n(n, max_delay)
+    end = time.time()
+    return (end - start) / n
